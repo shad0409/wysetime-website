95407d7c12b96d1e0a35197addb44eeb0ce38e41
shad0409 <shaad.0409@gmail.com>
2024-04-10 14:26:57 +0800

    favicon changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Navbar from "./Navbar";
 
 import ParticlesContainer from "@/components/ParticlesContainer";
 import Footer from "./Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?version=1"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+      </Head>
       <body className="mx-auto">
       <Navbar/>
       <ParticlesContainer/>
