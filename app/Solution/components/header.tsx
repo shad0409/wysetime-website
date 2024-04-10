a99a44f1a6eb90834ba6f1fec065011fcdd8a75a
shad0409 <shaad.0409@gmail.com>
2024-04-10 13:21:12 +0800

    complete draft

--- app/Solution/components/header.tsx
+++ app/Solution/components/header.tsx
@@ -2,47 +2,36 @@
 import React from 'react';
 import Image from 'next/image';
 
-import StoryIcon from '../../../public/OurStory.png';
-import VisionIcon from '../../../public/Vision.png';
-import TechIcon from '../../../public/Technology.png';
+import AISolution from '../../../public/AiSoftware.png';
+import Insights from '../../../public/Insights.png';
+import Data from '../../../public/Accuracy.png';
+import Scalability from '../../../public/Scalability.png';
 
 const SolutionHeader = () => {
   return (
     <>
       <div className="solution-items-container">
         <div className='solution-item-container'>
-            <Image src={StoryIcon} height={100} width={100} alt="Our Story" objectFit='contain' />
-            <h2>Our Story</h2>
-            <p>WyseTime brings e-commerce style analytics to your physical locations.
-            Improve your business efficiency by understanding your visitoractivity and receiving automated recommendations</p>
+            <Image src={AISolution} height={100} width={100} alt="Ai-Driven Solutions" objectFit='contain' />
+            <h2>AI-Driven Solutions</h2>
+            <p>Utilizing AI algorithms to analyze customer behavior, and personalize marketing strategies.</p>
         </div>
 
         <div className="solution-item-container">
-          <Image src={VisionIcon} height={100} width={100} alt="Our Vision" objectFit='contain' />
-          <h2>Our Vision</h2>
-          <p>WyseTime help brick-and-mortar businesses in creating 
-              superior customer experience. Through footfall analytics,
-              customer profile generation, personalized CRM 
-              engagement and in-depth business insights</p>
+          <Image src={Insights} height={100} width={100} alt="RTI" objectFit='contain' />
+          <h2>Real-Time Insights</h2>
+          <p>Providing real-time insights into customer preferences, helping retailers make decisions promptly.</p>
         </div>
 
         <div className="solution-item-container">
-          <Image src={TechIcon} height={100} width={100} alt="Technology" objectFit='contain' />
-          <h2>Technology</h2>
-          <p>WyseTime combines the transformative powers of 
-              Big Data and the Internet of Things (IoT). 
-              By using video analytic technology, 
-              we find a winning formula for retail analytics when people 
-              counting data is used for operational and strategic decisions.</p>
+          <Image src={Data} height={100} width={100} alt="ADE" objectFit='contain' />
+          <h2>Accurate Data Extraction</h2>
+          <p>Offering high accuracy in extracting data  using Optical Character Recognition (OCR) technology.</p>
         </div>
         <div className="solution-item-container">
-          <Image src={TechIcon} height={100} width={100} alt="Technology" objectFit='contain' />
-          <h2>Technology</h2>
-          <p>WyseTime combines the transformative powers of 
-              Big Data and the Internet of Things (IoT). 
-              By using video analytic technology, 
-              we find a winning formula for retail analytics when people 
-              counting data is used for operational and strategic decisions.</p>
+          <Image src={Scalability} height={100} width={100} alt="Scalability" objectFit='contain' />
+          <h2>Scalability</h2>
+          <p>Scaling to accommodate varying numbers of guests and ensuring smooth operations during peak periods.</p>
         </div>
       </div>
     </>
