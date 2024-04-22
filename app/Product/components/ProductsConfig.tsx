3aa891a119c044a93d86e5b2a721868481155cf3
shad0409 <shaad.0409@gmail.com>
2024-04-22 13:09:41 +0800

    css changes

--- app/Product/components/ProductsConfig.tsx
+++ app/Product/components/ProductsConfig.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
+import { PortableText } from 'next-sanity';
 
 interface ProductProps {
   image: any;
   title: string;
-  description: string;
+  description: any;
   reverse?: boolean; 
 }
 
@@ -17,14 +18,14 @@ const Products: React.FC<ProductProps> = ({ image, title, description, reverse }
         </div>
         <div className="product-content text-black">
           <h3>{title}</h3>
-          <p>{description}</p>
+          <PortableText value={description} />
         </div>
       </>
     ) : (
       <>
         <div className="product-content text-black">
           <h3>{title}</h3>
-          <p>{description}</p>
+          <PortableText value={description} />
         </div>
         <div className="product-image">
           <Image src={image} alt={title} width={500} height={500} objectFit='contain' />
