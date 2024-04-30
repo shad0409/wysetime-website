383826ca37371ca1f4207e544ce26b0303107854
shad0409 <shaad.0409@gmail.com>
2024-04-30 19:28:23 +0800

    navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -18,9 +18,13 @@ export default function Navbar() {
         <div className='max-w-10xl mx-auto px-4 sm:px-6 lg:px-8'>
             <div className='flex items-center justify-between h-16'>
                 <div className='flex items-center'>
-                    <div className='flex-shrink-0'>
+                <div className='flex-shrink-0'>
+                <Link href="/">
+                    <a>
                         <Image src={Logo} width={150} height={250} placeholder='blur' alt="logo"/>
-                    </div>
+                    </a>
+                </Link>
+                </div>
                 </div>
                 <div className='hidden md:block'>
                     <div className='ml-4 flex items-center space-x-4'>
