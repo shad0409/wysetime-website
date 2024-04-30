7c6bf498ccc3d449f97883ece4eca2876880a938
shad0409 <shaad.0409@gmail.com>
2024-04-30 14:39:16 +0800

    functioning contact form

--- app/Contact/components/Hero.tsx
+++ app/Contact/components/Hero.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import Image from 'next/image';
 import heroImage from '../../../public/Contact.jpeg';
 
 const ContactHeader = () => {
+  const [state, handleSubmit] = useForm("xrgnjppv");
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+  
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await handleSubmit(event);
+    setShowSuccessPopup(true);
+    setTimeout(() => {
+      window.location.reload(); 
+    }, 2000); 
+  };
+
     return (
         <div className="contact-container">
           <Image src={heroImage} alt="Empower Your Business" layout='fill' objectFit='cover' />
           <div className="contact-content">
             <h1>Contact Us</h1>
             <p>To schedule a product demo with our product consultants, please fill in your contact details</p>
-            <form className="contact-form">
-              <input type="text" id="full-name" placeholder="FULL NAME" />
-              <input type="email" id="email" placeholder="EMAIL" />
-              <input type="tel" id="contact-number" placeholder="CONTACT NUMBER" />
-              <textarea id="message" placeholder="MESSAGE"></textarea>
-              <button type="submit">Send Message</button>
+            <form onSubmit={handleFormSubmit} className="contact-form">
+            <input type="text" id="full-name" name="full-name" placeholder="FULL NAME" />
+            <input type="email" id="email" name="email" placeholder="EMAIL" />      
+            <ValidationError 
+              prefix="Email" 
+              field="email"
+              errors={state.errors}></ValidationError>
+            <input type="tel" id="contact-number" name="contact-number" placeholder="CONTACT NUMBER" />
+            <textarea id="message" name="message" placeholder="MESSAGE"></textarea>
+              <button type="submit" disabled={state.submitting}>Send Message</button>
             </form>
           </div>
+          {showSuccessPopup && (
+            <div className="success-popup">
+              <p>Successfully Submitted!</p>
+            </div>
+          )}
         </div>
       );
 };
